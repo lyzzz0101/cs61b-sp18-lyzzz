01ec7bf399650886f8f2cdc13bc0ea7afe448242
lyzzz0101 <74897248+lyzzz0101@users.noreply.github.com>
2021-11-05 13:02:00 +0800

    hw1

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -10,6 +10,13 @@ public class TestArrayRingBuffer {
     @Test
     public void someTest() {
         //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        BoundedQueue x = new ArrayRingBuffer(4);
+        x.enqueue(33.1); // 33.1 null null  null
+        x.enqueue(44.8); // 33.1 44.8 null  null
+        x.enqueue(62.3); // 33.1 44.8 62.3  null
+        x.enqueue(-3.4); // 33.1 44.8 62.3 -3.4
+        x.dequeue();
+        System.out.println(x);
     }
 
     /** Calls tests for ArrayRingBuffer. */
